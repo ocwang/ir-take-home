54d146303b0dbbce5d8f0c35b0778b3339096fd7
Chase Wang <hi@ocwang.com>
2018-11-06 11:31:47 -0800

    Initial Commit

---
+++ IRTakeHome/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IRTakeHome
+//
+//  Created by Chase Wang on 11/6/18.
+//  Copyright © 2018 ocw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
